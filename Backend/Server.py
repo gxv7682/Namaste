59bee2dc9fa666bc7647e82c62fb6014fe9b9b42
GV <gxv7682@g.rit.edu>
2014-05-23 22:02:15 -0400

    Update Server.py

--- Backend/Server.py
+++ Backend/Server.py
@@ -1,7 +1,10 @@
 from pymongo import MongoClient
 from bson.son import SON
+
 client = MongoClient()
-db = client['mydb']
+
+db = client['mydb'] #make sure u create a db named 'mydb' with a document 'location' ... use the 'location.json' for this
+
 iterator = db.location.find({"Postion" : SON([("$near",[9,10]),("$maxDistance",10)])})
 
 for locs in iterator:
